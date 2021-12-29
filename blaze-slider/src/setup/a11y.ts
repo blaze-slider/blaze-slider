76df505da00411afa06d86421111b896f59d30f4
Manan Tank <manantankm@gmail.com>
2021-12-29 23:45:12 +0530

    use destructuring in handleA11y

--- blaze-slider/src/setup/a11y.ts
+++ blaze-slider/src/setup/a11y.ts
@@ -3,17 +3,12 @@ import { BlazeSlider } from '../index'
 export function handleA11y(blazeSlider: BlazeSlider) {
   // a11y
   blazeSlider.slider.addEventListener('keydown', (event: KeyboardEvent) => {
-    if (
-      event.key === 'ArrowRight' ||
-      (event.key === 'Tab' && !event.shiftKey)
-    ) {
+    const { key, shiftKey } = event
+    if (key === 'ArrowRight' || (key === 'Tab' && !shiftKey)) {
       event.preventDefault()
       blazeSlider.swipeRight()
       blazeSlider.slides[blazeSlider.offset].focus()
-    } else if (
-      event.key === 'ArrowLeft' ||
-      (event.key === 'Tab' && event.shiftKey)
-    ) {
+    } else if (key === 'ArrowLeft' || (key === 'Tab' && shiftKey)) {
       event.preventDefault()
       blazeSlider.swipeLeft()
       blazeSlider.slides[blazeSlider.offset].focus()
