9fc7655d0df6b612c65c2c6e08f23897051a2b4f
Manan Tank <manantankm@gmail.com>
2021-12-31 18:07:34 +0530

    lint and add banner in dist files

--- blaze-slider/rollup.config.js
+++ blaze-slider/rollup.config.js
@@ -1,46 +1,67 @@
 import fs from 'fs'
+import banner from 'rollup-plugin-banner'
+import { eslintBundle } from 'rollup-plugin-eslint-bundle'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-ts'
 import pkg from './package.json'
 
 const minifier = terser({ compress: true })
 
+const eslintPlugin = eslintBundle({
+  eslintOptions: {
+    fix: true,
+  },
+  throwOnWarning: true,
+  throwOnError: true,
+  formatter: 'compact',
+})
+
+const bannerPlugin = banner(
+  'blaze-slider v<%= pkg.version %> by <%= pkg.author %>'
+)
+
 const outputs = [
   // esm
   {
     file: 'dist/blaze-slider.esm.js',
-    format: 'esm'
+    format: 'esm',
+    throwOnWarning: true,
+    plugins: [eslintPlugin, bannerPlugin],
   },
   // cjs.dev
   {
     file: 'dist/blaze-slider.cjs.dev.js',
-    format: 'cjs'
+    format: 'cjs',
+    plugins: [eslintPlugin, bannerPlugin],
   },
   // cjs.prod
   {
     file: 'dist/blaze-slider.cjs.prod.js',
     format: 'cjs',
-    plugins: [minifier]
+    plugins: [minifier, bannerPlugin],
   },
   // umd
   {
     file: 'dist/blaze-slider.umd.js',
     name: pkg.name,
-    format: 'umd'
-  }
+    format: 'umd',
+    plugins: [minifier, bannerPlugin],
+  },
 ]
 
-export default args => {
+export default (args) => {
   createCjsFile()
   return {
     input: './src/index.ts',
     output: args.dev ? outputs[0] : outputs,
-    plugins: [typescript()]
+    plugins: [typescript()],
   }
 }
 
 const createCjsFile = () => {
   const code = `\
+// blaze-slider v${pkg.version} by ${pkg.author}
+
 if (process.env.NODE_ENV !== 'production') {
   module.exports = require('./blaze-slider.cjs.dev.js');
 } else {
@@ -49,7 +70,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   fs.mkdirSync('./dist', { recursive: true })
 
-  fs.writeFile('./dist/blaze-slider.cjs.js', code, err => {
+  fs.writeFile('./dist/blaze-slider.cjs.js', code, (err) => {
     if (err) console.error(err)
   })
 }
