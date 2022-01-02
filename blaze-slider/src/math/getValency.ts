bae8be8471d26849d1c5af5f44e9710398c9ed6f
Manan Tank <manantankm@gmail.com>
2022-01-02 22:23:46 +0530

    remove unused files

--- blaze-slider/src/math/getValency.ts
+++
@@ -1,5 +0,0 @@
-import { getPageCount } from './getPageCount'
-
-export function getValency(slides: number, show: number, scroll: number) {
-  return getPageCount(slides, scroll) * scroll - slides
-}
