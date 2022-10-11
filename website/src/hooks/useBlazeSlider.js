be2d0c30f62effb412f687c99db5e2e782164ec6
Manan Tank <manantankm@gmail.com>
2022-10-11 18:23:19 +0530

    website: fix page crash of /initialize-slider

--- website/src/hooks/useBlazeSlider.js
+++ website/src/hooks/useBlazeSlider.js
@@ -10,7 +10,7 @@ export function useBlazeSlider(config) {
   const sliderRef = React.useRef()
   React.useEffect(() => {
     if (sliderRef.current.init) return
-    if (config.all && config.all && !config.all.transitionDuration) {
+    if (config && config.all && config.all && !config.all.transitionDuration) {
       config.all.transitionDuration = 300
     }
     new BlazeSlider(sliderRef.current, config)
