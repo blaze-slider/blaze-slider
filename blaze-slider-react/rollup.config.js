497c9107d60e59c2ddcc88e0e70c2505abc0318c
Manan Tank <manantankm@gmail.com>
2022-10-22 19:29:18 +0530

    update react-blaze-slider

--- blaze-slider-react/rollup.config.js
+++ blaze-slider-react/rollup.config.js
@@ -1,10 +1,12 @@
 import typescript from 'rollup-plugin-ts'
+import pkg from './package.json'
 
 export default {
   input: './useBlazeSlider.ts',
   plugins: [typescript()],
   external: ['react', 'blaze-slider'],
   output: {
+    banner: `/* ${pkg.name} v${pkg.version} by ${pkg.author} */`,
     file: 'dist/index.js',
     format: 'esm',
   },
